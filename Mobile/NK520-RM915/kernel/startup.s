6b6002ca4d9276bcba4355c27b7dc4acdc8b8c4d
Parsley Ohxi <km7bnv@proton.me>
2026-08-09 03:31:48 +0000

    idk what i changed (init entry?!?!??!!? iafdk)

--- Mobile/NK520-RM915/kernel/startup.s
+++ Mobile/NK520-RM915/kernel/startup.s
@@ -3,6 +3,8 @@
 
 .global _start
 .extern kernel_main
+.extern __bss_start
+.extern __bss_end
 
 
 .section .text
@@ -11,7 +13,18 @@
 _start:
     cpsid if
     ldr sp, =_stack_top
+
+    ldr r0, =__bss_start
+    ldr r1, =__bss_end
+    mov r2, #0
+
+bss_clear:
+    cmp r0, r1
+    strlo r2, [r0], #4
+    blo bss_clear
+
     bl kernel_main
+
 
 hang:
     b hang
