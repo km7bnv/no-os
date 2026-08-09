3207114b94da992419f7c4e403ec4bc1a3cb32a0
Parsley Ohxi <km7bnv@proton.me>
2026-08-09 03:21:40 +0000

    Edited startup.s with stack

--- Mobile/NK520-RM915/kernel/startup.s
+++ Mobile/NK520-RM915/kernel/startup.s
@@ -5,9 +5,22 @@
 .extern kernel_main
 
 
+.section .text
+
+
 _start:
+    ldr sp, =_stack_top
     bl kernel_main
 
 
 hang:
     b hang
+
+
+.section .bss
+.align 8
+
+_stack:
+    .space 4096
+
+_stack_top:
